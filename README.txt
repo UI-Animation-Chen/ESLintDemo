f50c44b956d8de8ce717144762b718b6c6e4c545
mtdp <mtdp@czf-MacBook-Pro.local>
2019-12-17 17:22:11 +0800

    init

---
+++ README.txt
@@ -0,0 +1,4 @@
+To make Node treat your file as a ES Module, you need to:
+1. set "type": "module" in the package.json;
+2. use the .mjs extension.
+
---
+++ main.mjs
@@ -0,0 +1,12 @@
+import recast from 'recast';
+
+const code = `
+    function add(a, b) {
+        return a + b;
+    }
+`;
+
+const ast = recast.parse(code);
+
+console.log(ast);
+
