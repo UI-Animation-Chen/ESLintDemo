685a370f313b228c9ad6a08262dd25e39dd1cb46
czf <chenzhifei03@meituan.com>
2020-02-09 21:22:39 +0800

    add mrn-base-utils eslint plugin

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ImageBackground } from 'react-native';
 
 export default class MyComponent extends React.PureComponent {
 
     render () {
         return (
             <View>
-                <Text style={{fontSize: 16, borderRadius: '1'}}>hello</Text>
+                <Text style={{fontSize: 16, borderRadius: 1}}>hello</Text>
                 <Text style={{fontSize: 15}}>hello</Text>
-                <Text style={{fontSize: 14}}>world</Text>
+                <ImageBackground style={{resizeMode: 'cover'}}>
+                    <Text style={{fontSize: 14}}>world</Text>
+                </ImageBackground>
             </View>
         );
     }
