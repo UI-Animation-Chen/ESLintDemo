15692a24ac63b9fc35bf886db42c826648523c5e
czf <chenzhifei03@meituan.com>
2020-03-15 22:13:33 +0800

    增加plugin:xxx/recommended的用法

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { View, Text, ImageBackground } from 'react-native';
+import { openUrl } from '@mrn/mrn-utils';
+
+const name = 'czf';
 
 export default class MyComponent extends React.PureComponent {
+
+    getName () {
+        /**openUrl('https://www.meituan.com', {
+            name: name,
+            age: 30
+        });*/
+        openUrl('https://www.meituan.com');
+    }
 
     render () {
         return (
             <View>
-                <Text style={{fontSize: 16, borderRadius: 1}}>hello</Text>
+                <Text style={{fontSize: 16, borderRadius: 1}}>hel<Text>jjsj</Text>lo</Text>
                 <Text style={{fontSize: 15}}>hello</Text>
-                <ImageBackground style={{resizeMode: 'cover'}}>
+                <ImageBackground imageStyle={{resizeMode: 'cover'}}>
                     <Text style={{fontSize: 14}}>world</Text>
                 </ImageBackground>
             </View>
